736bd64cba91a47db987f9ca8c26b47e729b8650
mojnooymeret <95657364+mojnooymeret@users.noreply.github.com>
2022-08-09 14:16:43 +0500

    Add files via upload

---
+++ freelanceparse/freelanceparse/QiwiData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace freelanceparse
+{
+   internal class QiwiData
+   {
+      public int id { get; set; }
+      public string title { get; set; }
+      public string mean { get; set; }
+      public QiwiData(int id, string title, string mean)
+      {
+         this.id = id;
+         this.title = title;
+         this.mean = mean;
+      }
+   }
+}
---
+++ freelanceparse/freelanceparse/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace freelanceparse
+{
+   internal class User
+   {
+      public int id { get; set; }
+      public string user_id { get; set; }
+      public string categories { get; set; }
+      public string sub { get; set; }
+      public User(int id, string user_id, string categories, string sub)
+      {
+         this.id = id;
+         this.user_id = user_id;
+         this.categories = categories;
+         this.sub = sub;
+      }
+   }
+}
